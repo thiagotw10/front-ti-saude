3fed78abad46463ce75adcfa82c572932bb58300
thiagotw10 <thiagotw10@hotmail.com>
2023-06-12 22:34:29 -0300

    feat: primeiro commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+
+  </q-page>
+  <div>
+    <q-link to="/new" @click="redirectToNewPage">Ir para outra rota</q-link>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  methods: {
+    redirectToNewPage() {
+      console.log('Clicou no link');
+      this.$router.push('/new');
+    }
+  }
+})
+</script>
